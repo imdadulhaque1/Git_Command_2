068e6bb94f2666cfeb72ea07f8db1c967ff3424a
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-01-23 01:43:39 +0600

    Initial Setup

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+    1. need to init the directory as "myApp": npm init
+    2. init as git repository: git init
+    3. git add .
+    4. git commit -m "Initial Setup"
+    5. git log --oneline
+    6. Install package: npm install bootstrap --save
+Need to ignore node_module, I create ".gitignore"
+    7. check git status: git status
+    8. git commit -m "Initialize Module"
+    9. 
+
+
+*/
